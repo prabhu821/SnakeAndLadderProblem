7f8030f32a0dc2d12ed4f87ab4ad2a3aaa55a618
rishiraj007 <rishiraj@mounttalent.com>
2023-02-27 18:40:21 +0530

    [Prabhu] Add: Start Position

---
+++ SnakeAndLadder/GamePlay.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder;
+
+public class GamePlay
+{
+    int rollDice, playerPosition = 0, playerPosition1 = 0, playerPosition2 = 0;
+    int count = 0;
+    public void Start()
+    {
+        Console.WriteLine("Game is Start:" + playerPosition);
+    }
+}
--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -4,6 +4,22 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("\nSnake And Ladder Problem");
+        bool flag = true;
+        while (flag)
+        {
+            Console.WriteLine("\nSnake And Ladder Problem");
+            Console.WriteLine("1.GameStart \n2.Exit");
+            int options = Convert.ToInt32(Console.ReadLine());
+            GamePlay game = new GamePlay();
+            switch (options)
+            {
+                case 1:
+                    game.Start();
+                    break;
+                default:
+                    flag = false;
+                    break;
+            }
+        }
     }
 }
